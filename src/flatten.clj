bf8b84c8d1a91b9c847fcd628c9f7f051a0ab816
Stathis Sideris <stathis@pixelated-noise.com>
2023-02-03 19:31:13 +0200

    Cleanup and add generic zipper iteration function

--- src/flatten.clj
+++ src/flatten.clj
@@ -9,10 +9,6 @@
     ["/baz" ["/quux" :quux] [["/qux0" :qux0] [["/qux1" :qux1]]]]
     ["/ba" ["/zz" ["/bazz" :bazz]] ["/baq" :baq]]]])
 
-(def the-routes
-  ["/yo"
-   ["/hey" :hh]])
-
 (defn flatten-routes-recursive [[path-part & [sec :as rst] :as all]]
   (cond (keyword? sec)      {path-part sec}
         (string? path-part) (update-keys (reduce merge (map flatten-routes-recursive rst))
@@ -31,39 +27,12 @@
 (defn flatten-routes-iter
   ([x] (flatten-routes-iter {} [] x))
   ([routes path [fst & rst :as returns]]
-   (prn (symbol "routes ") routes)
-   (prn (symbol "path   ") path)
-   (prn (symbol "returns") returns)
-   (prn (cond (empty? returns) (symbol "case 1")
-              (keyword? fst)   (symbol "case 2")
-              (string? fst)    (symbol "case 3")
-              (fn? fst)        (symbol "case 4")
-              :else            (symbol "case 5")))
-   (prn '------------------------------------------)
-   (cond (empty? returns) routes
-         (keyword? fst)   (recur (assoc routes (apply str path) fst) path rst)
-         (string? fst)    (recur routes (conj path fst) rst)
-         (fn? fst)        (recur routes (fst path) rst)
-         :else            (let [pop-marker (when (string? (first fst)) [pop])]
-                            (recur routes path (concat fst pop-marker rst))))))
-(defn flatten-routes-iter
-  ([x] (flatten-routes-iter {} [] x))
-  ([routes path [fst & rst :as returns]]
    (cond (empty? returns) routes
          (keyword? fst)   (recur (assoc routes (str/join path) fst) path rst)
          (string? fst)    (recur routes (conj path fst) rst)
          (fn? fst)        (recur routes (fst path) rst)
          :else            (let [pop-marker (when (string? (first fst)) [pop])]
                             (recur routes path (concat fst pop-marker rst))))))
-
-(defn flatten-routes-iter-compact
-  ([x] (flatten-routes-iter-compact {} [] x))
-  ([routes path [h & t :as returns]]
-   (cond (keyword? h) (recur (assoc routes (apply str path) h) path t)
-         (string? h)  (recur routes (conj path h) t)
-         (empty? h)   (if (empty? t) routes (recur routes (pop path) t))
-         :else        (let [pop-marker (when (string? (first h)) [[]])]
-                        (recur routes path (concat h pop-marker t))))))
 
 (defn flatten-routes-zipper [routes]
   (loop [res {} z (z/vector-zip routes)]
@@ -83,9 +52,23 @@
                             [(->> % z/path (map first) (filter string?) str/join) (z/node %)]))
        (into {})))
 
+(defn zipper-iteration [zipper vf]
+  (iteration identity
+             :initk zipper
+             :kf    z/next
+             :somef (complement z/end?)
+             :vf    vf))
+
+(defn flatten-routes-generic [routes]
+  (into {}
+        (zipper-iteration (z/vector-zip routes)
+                          #(when (-> % z/node keyword?)
+                             [(->> % z/path (map first) (filter string?) str/join) (z/node %)]))))
+
 (assert
  (= (flatten-routes-recursive the-routes)
     (flatten-routes-pre-1:11 the-routes)
     (flatten-routes-iter the-routes)
     (flatten-routes-zipper the-routes)
-    (flatten-routes-iteration the-routes)))
+    (flatten-routes-iteration the-routes)
+    (flatten-routes-generic the-routes)))
