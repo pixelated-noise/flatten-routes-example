6afa982789b0552ee8d2eb64411a7f657ce38627
Stathis Sideris <stathis@pixelated-noise.com>
2023-02-09 12:59:33 +0200

    Align formatting to blog post

--- src/flatten.clj
+++ src/flatten.clj
@@ -10,10 +10,15 @@
     ["/ba" ["/zz" ["/bazz" :bazz]] ["/baq" :baq]]]])
 
 (defn flatten-routes-recursive [[path-part & [sec :as rst] :as all]]
-  (cond (keyword? sec)      {path-part sec}
-        (string? path-part) (update-keys (reduce merge (map flatten-routes-recursive rst))
-                                         (partial str path-part))
-        :else               (reduce merge (map flatten-routes-recursive all))))
+  (cond (keyword? sec) ; form 1
+        {path-part sec}
+
+        (string? path-part) ; form 2
+        (update-keys (reduce merge (map flatten-routes-recursive rst))
+                     (partial str path-part))
+
+        :else ; form 3
+        (reduce merge (map flatten-routes-recursive all))))
 
 
 (defn flatten-routes-pre-1:11 [[path-part & [sec :as rst] :as all]]
@@ -27,21 +32,27 @@
 (defn flatten-routes-iter
   ([x] (flatten-routes-iter {} [] x))
   ([routes path [fst & rst :as returns]]
-   (cond (empty? returns) routes
-         (keyword? fst)   (recur (assoc routes (str/join path) fst) path rst)
-         (string? fst)    (recur routes (conj path fst) rst)
-         (fn? fst)        (recur routes (fst path) rst)
-         :else            (let [pop-marker (when (string? (first fst)) [pop])]
+   (cond (empty? returns) routes                                                ; 1
+         (keyword? fst)   (recur (assoc routes (str/join path) fst) path rst)   ; 2
+         (string? fst)    (recur routes (conj path fst) rst)                    ; 3
+         (fn? fst)        (recur routes (fst path) rst)                         ; 4
+         :else            (let [pop-marker (when (string? (first fst)) [pop])]  ; 5
                             (recur routes path (concat fst pop-marker rst))))))
 
 (defn flatten-routes-zipper [routes]
-  (loop [res {} z (z/vector-zip routes)]
-    (cond (z/end? z)            res
-          (keyword? (z/node z)) (recur (assoc res
-                                              (->> z z/path (map first) (filter string?) str/join)
-                                              (z/node z))
-                                       (z/next z))
-          :else                 (recur res (z/next z)))))
+  (loop [res {}
+         z   (z/vector-zip routes)]
+    (cond (z/end? z)
+          res
+
+          (keyword? (z/node z))
+          (recur (assoc res
+                        (->> z z/path (map first) (filter string?) str/join)
+                        (z/node z))
+                 (z/next z))
+
+          :else
+          (recur res (z/next z)))))
 
 (defn flatten-routes-iteration [routes]
   (->> (iteration identity
@@ -49,7 +60,8 @@
                   :kf    z/next
                   :somef (complement z/end?)
                   :vf    #(when (-> % z/node keyword?)
-                            [(->> % z/path (map first) (filter string?) str/join) (z/node %)]))
+                            [(->> % z/path (map first) (filter string?) str/join)
+                             (z/node %)]))
        (into {})))
 
 (defn zipper-iteration [zipper vf]
@@ -61,9 +73,10 @@
 
 (defn flatten-routes-generic [routes]
   (into {}
-        (zipper-iteration (z/vector-zip routes)
-                          #(when (-> % z/node keyword?)
-                             [(->> % z/path (map first) (filter string?) str/join) (z/node %)]))))
+        (zipper-iteration
+         (z/vector-zip routes)
+         #(when (-> % z/node keyword?)
+            [(->> % z/path (map first) (filter string?) str/join) (z/node %)]))))
 
 (assert
  (= (flatten-routes-recursive the-routes)
